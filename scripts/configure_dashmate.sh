02a1860dbd2987fe31f5c6ed13c771141e6bfdd9
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2021-07-22 18:45:36 +0300

    make scripts to configure test suite

--- scripts/configure_dashmate.sh
+++ scripts/configure_dashmate.sh
@@ -8,5 +8,6 @@ packages_path="$root_path/packages"
 DAPI_REPO_PATH="${packages_path}/dapi"
 DRIVE_REPO_PATH="${packages_path}/drive"
 
+${packages_path}/dashmate/bin/dashmate update -v
 ${packages_path}/dashmate/bin/dashmate config:set --config=${CONFIG_NAME} platform.dapi.api.docker.build.path $DAPI_REPO_PATH
 ${packages_path}/dashmate/bin/dashmate config:set --config=${CONFIG_NAME} platform.drive.abci.docker.build.path $DRIVE_REPO_PATH
--- scripts/setup_local.sh
+++ scripts/setup_local.sh
@@ -6,5 +6,6 @@ full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 root_path=$(dirname $dir_path)
 packages_path="$root_path/packages"
+tmp="$root_path/tmp"
 
-${packages_path}/dashmate/bin/dashmate setup ${CONFIG_NAME} --verbose --debug-logs --miner-interval="${MINING_INTERVAL_IN_SECONDS}s" --node-count=${MASTERNODES_COUNT}
+${packages_path}/dashmate/bin/dashmate setup ${CONFIG_NAME} --verbose --debug-logs --miner-interval="${MINING_INTERVAL_IN_SECONDS}s" --node-count=${MASTERNODES_COUNT} | tee ${root_path}/setup.log
